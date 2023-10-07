43ee938a25315611e71609fb96497eb67368cfdf
PostWild <147175532+PostWild@users.noreply.github.com>
2023-10-07 13:27:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """Hello {},
-My Name Is <a href=https://t.me/FastFinderBot>Fast Finder</a>,  Just Add Me To Your Group And Enjoy... """
+My Name Is <a href=https://t.me/FastFinderBot>Fast Finder</a>, This Bot Is Under Tasting Just Add Me To Your Group And Enjoy... """
     LZTHMB_TEXT = """Hello {},
 Glad to see you here. It seems that you really love <a href=https://t.me/LazyDeveloperr >LazyDeveloper's</a> work.\n\n<b>Thumbnail extracting</b> feature will be available soon, please join <a href=https://t.me/LazyDeveloper>Dev Channel</a> and stay tuned for next <a href=https://t.me/LazyDeveloper>update</a>.\n\n  🐞 Report Bug here: <a href=http://t.me/LazyDeveloperSupport>LazyDev Support</a>
     """
